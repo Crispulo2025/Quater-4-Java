441d25ac0ba1cb1aef23b7bc4d51146538b0cdbd
Crispulo2025 <marpalace1@gmail.com>
2026-05-15 07:00:00 +0800

    Add files via upload

--- MyFirstSwingApp.java
+++ MyFirstSwingApp.java
@@ -3,16 +3,16 @@ import javax.swing.JFrame;
 public class MyFirstSwingApp {
     public static void main(String[] args) {
 
-        // Create the JFrame
+        // Create a JFrame
         JFrame frame = new JFrame("My First Swing App");
 
-        // Set the size of the window
+        // Set size (width, height)
         frame.setSize(400, 300);
 
-        // Exit the application when the window is closed
+        // Exit application when window is closed
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Make the window visible
+        // Make the frame visible
         frame.setVisible(true);
     }
 }
